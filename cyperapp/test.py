9a6fd485e6dda96023ae0f17ecb8d27bd340c315
Đoàn Ngọc Dũng <154717139+Remstag@users.noreply.github.com>
2025-03-02 22:25:37 +0700

    cập nhật tính năng, tối ưu

--- cyperapp/test.py
+++ cyperapp/test.py
@@ -1,106 +1,115 @@
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import *
+import mhAES
 from tkinter import *
 from tkinter import  Button, filedialog
-import base64
 from importlib.metadata import entry_points
-KEy=b'KeyDayNeDuMaMayVoMaLay@1234ABCDX'
-IV=b'ThisIsAnIV123456'
-def mahoa(texxt):
-    texxt=str(texxt)
-    cipher=AES.new(KEy,AES.MODE_CBC,IV)
-    damahoa=cipher.encrypt(pad(texxt.encode(),AES.block_size))
-    return base64.b64encode(damahoa).decode()
-def giaima(mh):
-    cipher = AES.new(KEy, AES.MODE_CBC, IV)
-    giaimaa = base64.b64decode(mh.strip())
-    return unpad(cipher.decrypt(giaimaa), AES.block_size).decode("utf-8", errors="ignore").replace("\r\n", "\n")
-d=0
 widgets = []
-def check(val):
-    global d
-    d=val
-    update()
-
-def update():
-    global widgets
-    for widget in widgets:
-        widget.destroy()  # Xóa các widget đã tạo
-    widgets.clear()
-    if (d==1):
+win=Tk()
+frames = {}
+def show_frame(page):
+    frame = frames[page]
+    frame.tkraise()  # Đưa frame lên trên
+def mahoavb ():
+        frame = Frame(win)
+        frame.grid(row=0, column=0, sticky="nsew")
+        frame.grid_rowconfigure(0, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
+        def sett():
+            entry.delete("1.0","end")
+            entry.insert("end",output.get("1.0","end"))
         def get():
             text = entry.get("1.0", "end-1c")
-            text = mahoa(text)
+            text = mhAES.mahoa(text)
             output.delete("1.0", "end")
-            output.insert("end", text)
-        labelbg = Label(win, text="Nhập văn bản thử đi:", font=("Arial", 20))
-        labelbg.pack(pady=5)
-        entry = Text(win, wrap="word", height=10, width=50)
-        entry.pack(fill="x",padx=5, pady=5)
-        button = Button(win, text="Mã hóa", command=get)
-        button.pack(pady=5)
-        output = Text(win, wrap="word", height=10, width=50)
-        output.pack(fill="x",padx=5, pady=5)
-        widgets.extend([labelbg, entry, button, output])
-    elif d==2:
-        def get2():
-            text = entry.get("1.0", "end-1c")
-            text = giaima(text)
-            output.delete("1.0", "end")
-            output.insert("end", text)
-        labelbg = Label(win, text="Nhập bản mã đi:", font=("Arial", 20))
-        labelbg.pack(pady=5)
-        entry = Text(win, wrap="word", height=10, width=50)
-        entry.pack(fill="x",padx=5, pady=5)
-        button = Button(win, text="Giải mã", command=get2)
-        button.pack(pady=5)
-        output = Text(win, wrap="word", height=10, width=50)
-        output.pack(fill="x",padx=5, pady=5)
-        widgets.extend([labelbg, entry, button, output])
-    elif d==3:
+            output.insert("end", text+"\n")
         def mhfile():
             file_path = filedialog.askopenfilename(title="Chọn file để mã hóa")
             if file_path:
                 with open(file_path, "r",encoding="utf-8") as f:
                     data = f.read()
-                encrypted_data=mahoa(data)
-                output.delete("1.0", "end")
-
-                output.insert("end", encrypted_data+"\n")
-
-        labelbg = Label(win, text="Chọn file đi:", font=("Arial", 20))
+                entry.delete("1.0", "end")
+                entry.insert("end", data)
+        def savefile():
+            file_path=filedialog.asksaveasfilename(defaultextension="txt",
+                                                   filetypes=[("Text","*txt"),
+                                                              ("All file","*.*")])
+            if file_path:  # Nếu người dùng không hủy chọn file
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write(output.get("1.0", "end"))
+        labelbg =  Label(frame, text="Nhập văn bản thử đi:", font=("Arial", 20))
         labelbg.pack(pady=5)
-        button = Button(win, text="Chọn file để mã hóa", command=mhfile)
-        button.pack(pady=5)
-        output = Text(win, wrap="word", height=10, width=50)
-        output.pack(fill="x", padx=5, pady=5)
-        widgets.extend([labelbg, button, output])
-    elif d==4:
+        entry = Text(frame, wrap="word", height=10, width=50)
+        entry.pack(fill="x",padx=5, pady=5)
+        entry.pack(pady=5)
+        spacer = Label(frame, text=" ")  # Một label rỗng để tạo khoảng cách
+        spacer.pack(pady=5)
+        button = Button(frame, text="Mã hóa với AES", command=get)
+        button.place(x=300, y=224)
+        button = Button(frame, text="Nhập file", command=mhfile)
+        button.place(x=700, y=224)
+        button = Button(frame, text="Mã hóa tiếp", command=sett)
+        button.place(x=800, y=224)
+        button = Button(frame, text="Lưu vào file", command=savefile)
+        button.place(x=900, y=224)
+        output = Text(frame, wrap="word", height=10, width=50)
+        output.pack(fill="x",padx=5, pady=5)
+        frames["mhvb"] = frame
+def giaimavb():
+        frame = Frame(win)
+        frame.grid(row=0, column=0, sticky="nsew")
+        frame.grid_rowconfigure(0, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
         def gmfile():
-            file_path = filedialog.askopenfilename(title="Chọn file để giải mã")
+            file_path = filedialog.askopenfilename(title="Chọn file để mã hóa")
             if file_path:
-                with open(file_path, "rb") as f:
+                with open(file_path, "r",encoding="utf-8") as f:
                     data = f.read()
-                encrypted_data=giaima(data)
-                output.delete("1.0", "end")
-                output.insert("end", encrypted_data + "\n")
-
-        labelbg = Label(win, text="chọn file đi:", font=("Arial", 20))
+                entry.delete("1.0", "end")
+                entry.insert("end", data)
+        def sett():
+            entry.delete("1.0","end")
+            entry.insert("end",output.get("1.0","end"))
+        def get():
+            text = entry.get("1.0", "end-1c")
+            text = mhAES.giaima(text)
+            output.delete("1.0", "end")
+            output.insert("end", text)
+        def savefile():
+            file_path=filedialog.asksaveasfilename(defaultextension="txt",
+                                                   filetypes=[("Text","*txt"),
+                                                              ("All file","*.*")])
+            if file_path:  # Nếu người dùng không hủy chọn file
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write(output.get("1.0", "end"))
+        labelbg = Label(frame, text="Nhập bản mã đi:", font=("Arial", 20))
         labelbg.pack(pady=5)
-        button = Button(win, text="Chọn file để giải mã", command=gmfile)
-        button.pack(pady=5)
-        output = Text(win, wrap="word", height=10, width=50)
-        output.pack(fill="x", padx=5, pady=5)
-        widgets.extend([labelbg, button, output])
-win=Tk()
+        entry = Text(frame, wrap="word", height=10, width=50)
+        entry.pack(fill="x",padx=5, pady=5)
+        spacer = Label(frame, text=" ")  # Một label rỗng để tạo khoảng cách
+        spacer.pack(pady=5)
+        button = Button(frame, text="Giải mã với AES", command=get)
+        button.place(x=300, y=224)
+        button = Button(frame, text="Nhập file", command=gmfile)
+        button.place(x=700, y=224)
+        button = Button(frame, text="Giải mã tiếp", command=sett)
+        button.place(x=800, y=224)
+        button = Button(frame, text="Lưu vào file", command=savefile)
+        button.place(x=900, y=224)
+        output = Text(frame, wrap="word", height=10, width=50)
+        output.pack(fill="x",padx=5, pady=5)
+        frames["gmvb"] = frame
 win.title("Test")
-win.geometry("1000x750")
-b1 = Button(win, text="Mã hóa", command=lambda:check(1))
-b1.place(x=250, y=600)
-b2 = Button(win, text="Giải mã", command=lambda:check(2))
-b2.place(x=350, y=600)
-b3 = Button(win, text="Mã hóa file", command=lambda:check(3))
-b3.place(x=450, y=600)
-b4 = Button(win, text="Giải mã file", command=lambda:check(4))
-b4.place(x=550, y=600)
+win.geometry("1000x650")
+win.grid_rowconfigure(0, weight=1)
+win.grid_columnconfigure(0, weight=1)
+mahoavb()
+giaimavb()
+butframe = Frame(win)
+butframe.grid(row=1, column=0, sticky="ew", pady=10)
+spacer = Label(butframe, text=" ")  # Một label rỗng để tạo khoảng cách
+spacer.pack(side="left", padx=200, pady=100)
+b1 = Button(butframe, text="Mã hóa", command=lambda: show_frame("mhvb"))
+b1.pack(side="left", padx=10, pady=100)
+b2 = Button(butframe, text="Giải mã", command=lambda: show_frame("gmvb"))
+b2.pack(side="left", padx=10, pady=5)
+show_frame("mhvb")
 win.mainloop()
