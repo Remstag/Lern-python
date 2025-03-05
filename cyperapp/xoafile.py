ceca050ec17eff0088b6e78c3c1fcdb9da58e269
Đoàn Ngọc Dũng <154717139+Remstag@users.noreply.github.com>
2025-03-05 16:04:29 +0700

    chức năng xóa file an toàn

---
+++ cyperapp/xoafile.py
@@ -0,0 +1,31 @@
+import random
+import os
+quyen=""
+status="❌ Xóa file thất bại"
+def checkquyen(file_path):
+    print(file_path)
+    global quyen
+    if not os.path.exists(file_path):
+        quyen= "❌ File không tồn tại!"
+        return False
+    if not os.access(file_path, os.W_OK):
+        quyen= "🚫 Không có quyền xóa file! Thử chạy với admin."
+        return False
+    quyen= "✅ Bạn có quyền xóa file này!"
+    return True
+def xoafilee(file_path):
+    global status
+    size=os.path.getsize(file_path)
+    with open(file_path, "wb") as f:
+        f.write(b'\x00' * size) #lan 1
+        f.flush()
+
+        f.write(b'xFF'*size) # lan 2
+        f.flush()
+
+        f.seek(0)
+        f.write(os.urandom(size))
+        f.flush()
+    os.remove(file_path)
+    status="✅ Xóa file thành công"
+
