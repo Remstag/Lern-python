455ae91b9c8f6637e9d93274d1b6feb238a6b003
heiuhoccode <danghieu121020@gmail.com>
2025-03-10 13:37:20 +0700

    tach file

---
+++ Hieu/main_Hieu.py
@@ -0,0 +1,9 @@
+
+from Hieu import Graphic
+from Hieu import MaHoaBlowFish
+from Hieu import GiaiMaBlowFish
+Graphic.setup()
+MaHoaBlowFish.mahoavb()
+GiaiMaBlowFish.giaimavb()
+Graphic.inteface()
+
