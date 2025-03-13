234c143013b9a88561b5e988a36fc190fa7cdf31
Đoàn Ngọc Dũng <154717139+Remstag@users.noreply.github.com>
2025-03-13 22:58:46 +0700

    update 3des

--- cyperapp/test.py
+++ cyperapp/test.py
@@ -13,7 +13,6 @@ import xoafile
 
 #cac bien toan cuc
 giatricu=""
-
 widgets = []
 win=Tk()
 frames = {}
@@ -427,6 +426,216 @@ def giaimavb():
 
         frames["gmvb"] = frame
 
+def mahoavb3des():
+    frame = Frame(win)
+    frame.grid(row=0, column=0, sticky="nsew")
+    frame.grid_rowconfigure(0, weight=1)
+    frame.grid_columnconfigure(0, weight=1)
+
+    def sett():
+        entry.delete("1.0", "end")
+        entry.insert("end", output.get("1.0", "end"))
+
+    def settlaigiatri():
+        entry.delete("1.0", "end")
+        entry.insert("end", giatricu)
+
+    def tamluu():
+        global giatricu
+        giatricu = output.get("1.0", "end")
+    def get():
+        try:
+            keyy = entry_key.get("1.0", "end").strip()
+            keyb = keyy.encode("utf-8")
+            iiv = entry_iv.get("1.0", "end").strip()
+            ivb = iiv.encode("utf-8")
+            text = entry.get("1.0", "end-1c")
+            text = BlowFish.mahoa(text, keyb, ivb)
+            output.delete("1.0", "end")
+            output.insert("end", text)
+        except Exception as e:
+            output.delete("1.0", "end")
+            output.insert("end", "Something wrong")
+
+    def setkey():
+        key_length = random.randint(4, 56)
+        entry_key.delete("1.0", "end")
+        random_key = ''.join(random.choices(string.ascii_letters + string.digits, k=key_length))
+        entry_key.insert("end", random_key)
+
+    def setiv():
+        entry_iv.delete("1.0", "end")
+        random_iv = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+        entry_iv.insert("end", random_iv)
+
+    def mhfile():
+        file_path = filedialog.askopenfilename(title="Chọn file để mã hóa")
+        if file_path:
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = f.read()
+            entry.delete("1.0", "end")
+            entry.insert("end", data)
+
+    def savefile():
+        file_path = filedialog.asksaveasfilename(defaultextension="txt",
+                                                 filetypes=[("Text", "*txt"),
+                                                            ("All file", "*.*")])
+        if file_path:
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write(output.get("1.0", "end"))
+
+    label_frame = Frame(frame)
+    label_frame.pack(side="top", fill="x")  # Dùng fill="x" để giãn đều
+
+    label_key = Label(label_frame, text="Nhập key từ 4 đến 56 ký tự:", font=("Arial", 20))
+    label_key.pack(side="left", expand=True)
+
+    label_iv = Label(label_frame, text="Nhập IV đủ 8 kí tự:", font=("Arial", 20))
+    label_iv.pack(side="left", expand=True)
+
+    text_frame = Frame(frame)
+    text_frame.pack(side="top", fill="x", pady=5)
+
+    entry_key = Text(text_frame, wrap="word", height=2, width=60)
+    entry_key.pack(side="left", expand=True, padx=5)
+
+    entry_iv = Text(text_frame, wrap="word", height=2, width=60)
+    entry_iv.pack(side="left", expand=True, padx=5)
+    spacer = Label(frame, text=" ")  # Một label rỗng để tạo khoảng cách
+    spacer.pack(pady=5)
+    button = Button(frame, text="Tạo key tự động", command=setkey)
+    button.place(x=200, y=90)
+    button = Button(frame, text="Tạo IV tự động", command=setiv)
+    button.place(x=700, y=90)
+    labelbg = Label(frame, text="Nhập văn bản thử đi:", font=("Arial", 20))
+    labelbg.pack(pady=5)
+
+    entry = Text(frame, wrap="word", height=10, width=50)
+    entry.pack(fill="x", padx=5, pady=5)
+    entry.pack(pady=5)
+
+    spacer = Label(frame, text=" ")  # Một label rỗng để tạo khoảng cách
+    spacer.pack(pady=5)
+
+    button = Button(frame, text="Mã hóa với 3-DES", command=get)
+    button.place(x=300, y=340)
+
+    button = Button(frame, text="Lấy lại giá trị", command=settlaigiatri)
+    button.place(x=500, y=340)
+
+    button = Button(frame, text="Tạm lưu", command=tamluu)
+    button.place(x=600, y=340)
+
+    button = Button(frame, text="Nhập file", command=mhfile)
+    button.place(x=700, y=340)
+
+    button = Button(frame, text="Mã hóa tiếp", command=sett)
+    button.place(x=800, y=340)
+
+    button = Button(frame, text="Lưu vào file", command=savefile)
+    button.place(x=900, y=340)
+
+    output = Text(frame, wrap="word", height=10, width=50)
+    output.pack(fill="x", padx=5, pady=5)
+
+    frames["mhvb3des"] = frame
+def giaimavb():
+    frame = Frame(win)
+    frame.grid(row=0, column=0, sticky="nsew")
+    frame.grid_rowconfigure(0, weight=1)
+    frame.grid_columnconfigure(0, weight=1)
+
+    def gmfile():
+        file_path = filedialog.askopenfilename(title="Chọn file để giải mã")
+        if file_path:
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = f.read()
+            entry.delete("1.0", "end")
+            entry.insert("end", data)
+
+    def sett():
+        entry.delete("1.0", "end")
+        entry.insert("end", output.get("1.0", "end"))
+
+    def settlaigiatri():
+        entry.delete("1.0", "end")
+        entry.insert("end", giatricu)
+
+    def tamluu():
+        global giatricu
+        giatricu = output.get("1.0", "end")
+
+    def get():
+        try:
+            keyy = entry_key.get("1.0", "end").strip()
+            keyb = keyy.encode("utf-8")
+            iiv = entry_iv.get("1.0", "end").strip()
+            ivb = iiv.encode("utf-8")
+            text = entry.get("1.0", "end-1c")
+            text = BlowFish.giaima(text, keyb, ivb)
+            output.delete("1.0", "end")
+            output.insert("end", text)
+        except Exception as e:
+            output.delete("1.0", "end")
+            output.insert("end", "Something Wrong")
+
+    def savefile():
+        file_path = filedialog.asksaveasfilename(defaultextension="txt",
+                                                 filetypes=[("Text", "*txt"),
+                                                            ("All file", "*.*")])
+        if file_path:
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write(output.get("1.0", "end"))
+
+    label_frame = Frame(frame)
+    label_frame.pack(side="top", fill="x")  # Dùng fill="x" để giãn đều
+
+    label_key = Label(label_frame, text="Nhập key từ 4 đến 56 ký tự:", font=("Arial", 20))
+    label_key.pack(side="left", expand=True)
+
+    label_iv = Label(label_frame, text="Nhập IV đủ 8 kí tự:", font=("Arial", 20))
+    label_iv.pack(side="left", expand=True)
+
+    text_frame = Frame(frame)
+    text_frame.pack(side="top", fill="x", pady=5)
+
+    entry_key = Text(text_frame, wrap="word", height=2, width=60)
+    entry_key.pack(side="left", expand=True, padx=5)
+
+    entry_iv = Text(text_frame, wrap="word", height=2, width=60)
+    entry_iv.pack(side="left", expand=True, padx=5)
+
+    labelbg = Label(frame, text="Nhập bản mã đi:", font=("Arial", 20))
+    labelbg.pack(pady=5)
+
+    entry = Text(frame, wrap="word", height=10, width=50)
+    entry.pack(fill="x", padx=5, pady=5)
+
+    spacer = Label(frame, text=" ")  # Một label rỗng để tạo khoảng cách
+    spacer.pack(pady=5)
+
+    button = Button(frame, text="Giải mã với 3-DES", command=get)
+    button.place(x=300, y=310)
+
+    button = Button(frame, text="Lấy lại giá trị", command=settlaigiatri)
+    button.place(x=500, y=310)
+
+    button = Button(frame, text="Tạm lưu", command=tamluu)
+    button.place(x=600, y=310)
+
+    button = Button(frame, text="Nhập file", command=gmfile)
+    button.place(x=700, y=310)
+
+    button = Button(frame, text="Giải mã tiếp", command=sett)
+    button.place(x=800, y=310)
+
+    button = Button(frame, text="Lưu vào file", command=savefile)
+    button.place(x=900, y=310)
+
+    output = Text(frame, wrap="word", height=10, width=50)
+    output.pack(fill="x", padx=5, pady=5)
+
+    frames["gmvb3des"] = frame
 def xoafil():
     frame=Frame(win)
     frame.grid(row=0, column=0, sticky="nsew")
@@ -523,12 +732,13 @@ win.grid_rowconfigure(0, weight=1)
 win.grid_columnconfigure(0, weight=1)
 mahoavbbf()
 giaimavbbf()
+mahoavb3des()
 mahoavb()
 giaimavb()
 xoafil()
 kiemtrafile()
 butframe = Frame(win)
-butframe.grid(row=2, column=0, columnspan=3, pady=80)
+butframe.grid(row=2, column=0, columnspan=3, pady=90)
 b1 = Button(butframe, text="Mã hóa với AES", command=lambda: show_frame("mhvb"))
 b1.grid(row=0, column=0, padx=10, pady=5)
 
@@ -546,6 +756,12 @@ b5.grid(row=1, column=0, padx=10, pady=5)
 
 b6= Button(butframe, text="Giải mã với BlowFish", command=lambda: show_frame("gmvbBF"))
 b6.grid(row=1, column=1, padx=10, pady=5)
+
+b7 = Button(butframe, text="Mã hóa với 3-DES", command=lambda: show_frame("mhvb3des"))
+b7.grid(row=1, column=2, padx=10, pady=5)
+
+b8 = Button(butframe, text="Giải mã với 3-DES", command=lambda: show_frame("gmvb3des"))
+b8.grid(row=1, column=3, padx=10, pady=5)
 
 show_frame("mhvb")
 win.mainloop()
