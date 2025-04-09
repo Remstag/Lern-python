63e2b6a1b604e92713bb457e2bc81459935561ba
Đoàn Ngọc Dũng <154717139+Remstag@users.noreply.github.com>
2025-04-09 20:40:56 +0700

    update

--- Bannop/Graphic/KiemTraTinhToanVen/checktv.py
+++ Bannop/Graphic/KiemTraTinhToanVen/checktv.py
@@ -36,6 +36,25 @@ def kiemtrafile(main_content):
             outputt.insert("end", "❌Có lỗi xảy ra!")
         # Frame include nhapvanban, vanbanmahoa, tinhnang
 
+    def checktvmb():
+        file_path1 = inputt1.get("1.0", "end").strip()
+        file_path1 = file_path1.replace("/", "\\\\")
+        file_path2 = inputt2.get("1.0", "end").strip()
+        file_path2 = file_path2.replace("/", "\\\\")
+        hash_goc = cheeck.travevb(file_path1)
+        hash_cancheck = cheeck.shaa256(file_path2)
+        if (hash_goc and hash_cancheck):
+            if hash_goc == hash_cancheck:
+                outputt.delete("1.0", "end")
+                outputt.insert("end", "✅ File không bị thay đổi.")
+
+            else:
+                outputt.delete("1.0", "end")
+                outputt.insert("end", "❌ File đã bị thay đổi!")
+
+        else:
+            outputt.delete("1.0", "end")
+            outputt.insert("end", "❌Có lỗi xảy ra!")
     frame2 = Frame(frame, bg="violet", padx=2, pady=2, bd=1, relief="solid")
     frame2.grid(row=0, column=0, sticky="nsew")
     frame2.grid_rowconfigure(0, weight=1)
@@ -52,7 +71,7 @@ def kiemtrafile(main_content):
     nhapvanban_frame.grid_columnconfigure(2, weight=1)
     Frame_label_nvb = Frame(nhapvanban_frame)
     Frame_label_nvb.grid(row=1, column=1, sticky="ew")
-    label_nvb = Label(Frame_label_nvb, text="File gốc", font=("Arial", 20))
+    label_nvb = Label(Frame_label_nvb, text="File dữ liệu gốc/File mã băm gốc", font=("Arial", 20))
     label_nvb.pack(side="left", fill="y")
 
     inputt1 = Text(nhapvanban_frame, wrap="word", height=6)
@@ -74,15 +93,17 @@ def kiemtrafile(main_content):
     for i in range(3):
         tinhnang_frame.grid_columnconfigure(i, weight=1)
 
-    button = Button(tinhnang_frame, text="Nhập file gốc", font=("Arial", 14), command=lambda: getfile(inputt1))
+    button = Button(tinhnang_frame, text="Nhập File gốc/File mã băm gốc", font=("Arial", 14), command=lambda: getfile(inputt1))
     button.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
 
     button = Button(tinhnang_frame, text="Nhập file cần kiểm tra", font=("Arial", 14), command=lambda: getfile(inputt2))
     button.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
 
-    button = Button(tinhnang_frame, text="Kiểm tra toàn vẹn", font=("Arial", 14), command=checktv)
+    button = Button(tinhnang_frame, text="Kiểm tra với file dữ liệu gốc", font=("Arial", 14), command=checktv)
     button.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
 
+    button = Button(tinhnang_frame, text="Kiểm tra vói file mã băm gốc", font=("Arial", 14), command=checktvmb)
+    button.grid(row=3, column=1, padx=5, pady=5, sticky="nsew")
     # Frame include vanbanmahoa
     vanbanmahoa_frame = Frame(frame2, padx=2, pady=20, bd=1, relief="solid")
     vanbanmahoa_frame.grid(row=1, column=0, sticky="nsew")
