858602f26a96fd433ae77d4a80f7f507b36b2089
Đoàn Ngọc Dũng <154717139+Remstag@users.noreply.github.com>
2025-04-12 15:52:32 +0700

    update

--- Bannop/Graphic/Database.py
+++ Bannop/Graphic/Database.py
@@ -8,8 +8,6 @@ def get_db_connection():
 def init_db():
     conn = get_db_connection()
     cursor = conn.cursor()
-
-
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id_user INTEGER PRIMARY KEY AUTOINCREMENT,
