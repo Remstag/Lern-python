e8cbb339a00000f397423d1e88b9277e5cfe461f
Đoàn Ngọc Dũng <154717139+Remstag@users.noreply.github.com>
2025-04-16 20:36:57 +0700

    Add files via upload

--- Graphic/KiemTraTinhToanVen/taomabam.py
+++ Graphic/KiemTraTinhToanVen/taomabam.py
@@ -58,7 +58,7 @@ def xoafil(main_content):
                     return
 
             try:
-                if giatricuu.mahoa == 1:
+                if giatricuu.mahoa == 1 and selected_path.startswith(appne_dir):
                     key = simpledialog.askstring("Nhập key", "File đang được mã hóa. Nhập key để giải mã:")
                     if not key:
                         messagebox.showinfo("Hủy thao tác", "Bạn đã hủy việc giải mã.")
