d47b423b4d01acec844c768f56a522711619a34c
Đoàn Ngọc Dũng <154717139+Remstag@users.noreply.github.com>
2025-04-16 20:37:34 +0700

    Add files via upload

--- Graphic/TripleDES/TripleDES_Decode.py
+++ Graphic/TripleDES/TripleDES_Decode.py
@@ -60,7 +60,7 @@ def giaimavb3des(main_content):
 
                 # ==== Biến xác định trạng thái file (đã giải mã chưa?) ====
 
-                if giatricuu.mahoa == 1:
+                if giatricuu.mahoa == 1 and selected_path.startswith(appne_dir):
                     key = simpledialog.askstring("Nhập key", "File đang được mã hóa. Nhập key để giải mã:")
                     if not key:
                         messagebox.showinfo("Hủy thao tác", "Bạn đã hủy việc giải mã.")
@@ -112,7 +112,7 @@ def giaimavb3des(main_content):
 
             try:
 
-                if giatricuu.mahoa == 1:
+                if giatricuu.mahoa == 1 and selected_path.startswith(appne_dir):
                     key = simpledialog.askstring("Nhập key", "Thư mục đang được mã hóa. Nhập key để giải mã:")
                     if not key:
                         messagebox.showinfo("Hủy thao tác", "Bạn đã hủy việc giải mã.")
